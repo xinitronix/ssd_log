40a0801cba831e2698790018a1778c8f5d11612e
Your Name <you@example.com>
2025-03-26 09:41:36 +0300

    изменено:      2025-03-26.log 	изменено:      ssd.sh

--- ssd.sh
+++ ssd.sh
@@ -36,3 +36,12 @@ disk="
 }
 
 get_TBW
+
+dir=$(dirname "$(realpath $0)")
+cd $dir
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git add --all
+git add .
+git commit -n
+git push ssh://git@github.com/xinitronix/ssd_log.git
